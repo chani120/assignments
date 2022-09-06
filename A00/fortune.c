350c0d7fbeda764168cb6ad2b748cb1cc295171d
Chandini <44120884+chani120@users.noreply.github.com>
2022-09-06 17:04:20 -0400

    Update fortune.c

--- A00/fortune.c
+++ A00/fortune.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  printf("Something exciting will ahppen soon!");
-  return 0;
-}
-#include <stdio.h>
-
-int main() {
   printf("Something exciting will come you way very soon!");
   return 0;
 }
