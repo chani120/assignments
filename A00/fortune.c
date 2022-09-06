eeda0bbb0b4b929e71981a8e06329215a78f161e
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-09-06 17:03:33 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Something exciting will ahppen soon!");
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  printf("Something exciting will come you way very soon!");
+  return 0;
+}
