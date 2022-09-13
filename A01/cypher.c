da636136b6a4435dd3147c629fb340a2897a123f
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-09-13 15:55:32 -0400

    wip

--- A01/cypher.c
+++ A01/cypher.c
@@ -1,3 +1,15 @@
+#include <stdlib.h> //includes rand() function
+#include <stdio.h> //includes printf
+
+void cypher(char input, int shift){
+  input 
+}
+
+
 int main() {
+  char word[];
+  int x;
+  cypher(word,x);
+  printf("Your cypher is:",%s,word);
   return 0;
 }
