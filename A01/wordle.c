10120080350fffc27c87d85b0ebcbac6f614c693
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-09-17 02:38:44 -0400

    completed

--- A01/wordle.c
+++ A01/wordle.c
@@ -4,33 +4,30 @@
 #include <time.h>
 #include "words.h" // header that defined chooseWord()
 
-int main() {
-  srand(time(0));
-  const char* word = chooseWord();
- 
-  char guess[5];
+int wordle(const char* w){
+char guess[5];
   
   int count = 0;
   while ( count<6){
     printf("Please enter a 5-letter word: ");
     scanf(" %s", guess);
 
-    if (strcmp(guess,word)==0){
+    if (strcmp(guess,w)==0){
       printf("You guessed it! The word is %s ", guess);
       count = 6;
     }
 
-    else if (strcmp(guess,word)!=0){
+    else if (strcmp(guess,w)!=0){
       for (int i=0; i<5; i++){
-        if(guess[i] == word[i]){
+        if(guess[i] == w[i]){
             printf("%c is in the word AND in the correct spot!\n", guess[i]);
         }
 
-        else if(strchr(word, guess[i]) != NULL && i!=2){
+        else if(strchr(w, guess[i]) != NULL){
             printf("%c is in the word (but not in the correct spot)!\n", guess[i]);
         }
 
-        else if(strchr(word, guess[i]) == NULL ){
+        else if(strchr(w, guess[i]) == NULL ){
             printf("%c is NOT in the word.\n", guess[i]);
         }  
       }
@@ -40,5 +37,15 @@ int main() {
     count = count + 1;
   }
 
+  if (strcmp(guess,w)!=0){
+  printf("You Lost! The word was:%s ", w);
+  }
+   return 0;
+}
+
+int main() {
+  srand(time(0));
+  const char* word = chooseWord();
+  wordle(word);
   return 0;
 }
