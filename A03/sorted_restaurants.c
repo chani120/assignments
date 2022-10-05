2c60ecae07913a57da846df1ded573634080936f
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-10-04 20:12:12 -0400

    wip

--- A03/sorted_restaurants.c
+++ A03/sorted_restaurants.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct restaurants{
     char name[64];
     int opening_hour;
@@ -10,11 +9,9 @@ struct restaurants{
 };
 
 struct node {
-    struct restaurants res_list;
-    struct restaurants data;           
+    struct restaurants res_list;       
     struct node *next;
 } ;
-
 
 struct node* insert_front(struct restaurants res_list, struct node* head){
     struct node* n = malloc(sizeof(struct node));
@@ -27,7 +24,6 @@ struct node* insert_front(struct restaurants res_list, struct node* head){
     return n;
 };
 
-
 void print(struct node*list)
 {
    for(struct node* n = list; n!=NULL; n=n->next){
@@ -36,26 +32,55 @@ void print(struct node*list)
    }
 }
 
-struct data{
-    printf("Whats the name of the restaurant?\n");
-    scanf("%s", restaurants.name);
-    printf("How well liked is this restaurant?(rating)\n");
-    scanf("%g", restaurants.rating);
-    printf("When does the restaurant open?\n");
-    scanf("%d", restaurants.opening_hour);
-    printf("When does the restaurant close?\n");
-    scanf("%d",restaurants.closing_hour);
-};
+
 
 int main(){
     struct node* head = NULL;
     int num_res;
     printf("How many restuaraunts?\n");
     scanf("%d",&num_res);
-    struct restaurants data;
-    for(int i=0; i<=num_res; i++){
-    head = insert_front(data,head);
+    struct restaurants *data;
+    data = malloc(sizeof(struct restaurants)*num_res);
+
+    for(int i=0; i<num_res; i++){
+    printf("Whats the name of the restaurant?\n");
+    scanf("%s", data[i].name);
+    printf("How well liked is this restaurant?(rating)\n");
+    scanf("%g", &data[i].rating);
+    printf("When does the restaurant open?\n");
+    scanf("%d", &data[i].opening_hour);
+    printf("When does the restaurant close?\n");
+    scanf("%d",&data[i].closing_hour);
     }
-    printf(head);
+    
+    float arr[num_res];
+
+    for(int i=0; i < num_res; i++){
+        arr[i]= data[i].rating;
+    }
+
+ 
+    int temp = 0;
+    for(int i=0; i < num_res; i++){
+        for(int j= 1; j < num_res; j++){
+            if (arr[i] < arr[j]){
+                temp = arr[i];       
+                arr[i] = arr[j];     
+                arr[j] = temp;
+            }    
+        }   
+    }
+    printf("%g",arr);
+    
+    for(int i=0; i < num_res; i++){
+        for(int j= 0; j < num_res; j++){
+            if(arr[i]==data[j].rating){
+            head = insert_front(data[j],head);
+            }
+        }
+    }
+
+
+    print(head);
     return 0;
  }
