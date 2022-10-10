02dd2c754e73d1467e9a3530d4eb15d5a2fe600c
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-10-10 17:46:11 -0400

    wip

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -3,12 +3,40 @@
 #include <string.h>
 #include "read_ppm.h"
 
-// Choose *one* to implement
+
+struct ppm_pixel {
+  unsigned char red;
+  unsigned char green;
+  unsigned char blue;
+}
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE* fp = fopen(filename, "rb");
+  char str[100];
+  char check[2];
+  
+  if(fp == NULL) {
+      perror("Error opening file");
+      return(NULL);
+   }
+  
+  fgets(str,100, fp);
+  sscanf(str, "%s", check);
+  if( check[0] != 'P' || check[1] != '6') {
+      printf("File is not valid");
+      return NULL;
+   }
+  
+  fgets(str, 100, fp);
+  sscanf(str, "%d %d", w,h);
+  
+  struct ppm_pixel* arr = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+  for(int i = 0; 1 <*h; i++){
+    for(int j = 0; 1 <*w; j++){
+      fscanf(fp, " %hhu %hhu %hhu", &arr[i*(*h)+j].red, &arr[i*(*h)+j].green, &arr[i*(*h)+j].blue);
+    }
+  }
+  fclose(fp);
+  return(arr);
 }
 
-struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
-}
