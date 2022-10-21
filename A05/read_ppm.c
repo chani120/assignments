82093c55c2540d712a95d12a53eb3b04f93ab896
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-10-20 22:34:10 -0400

    done

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -3,12 +3,39 @@
 #include <string.h>
 #include "read_ppm.h"
 
-// implement one
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE* fp = fopen(filename, "rb");
+  char str[100];
+  char check[2];
+
+  
+  if(fp == NULL) {
+      perror("Error opening file");
+      return(NULL);
+   }
+  
+  fgets(str,100, fp);
+  printf("%s",str);
+  sscanf(str, "%s", check);
+  if( check[0] != 'P' || check[1] != '6') {
+      printf("File is not valid");
+      return NULL;
+   }
+  
+  fgets(str, 100, fp);
+  printf("%s",str);
+  fgets(str, 100, fp);
+  sscanf(str, "%d %d",w,h);
+  printf("%d %d",*w,*h);
+  fgets(str, 100, fp);
+  printf("%s",str);
+
+  struct ppm_pixel* arr = malloc(sizeof(struct ppm_pixel)*(*w)*(*h));
+
+  fread(arr, sizeof(struct ppm_pixel),(*w)*(*h), fp);
+  fclose(fp);
+  return(arr);
+  free(arr);
 }
 
-struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
-}
