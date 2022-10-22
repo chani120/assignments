fa18bba25de7947b23af2a9b14270960ef19a1b0
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-10-22 19:51:13 -0400

    completed

--- A05/bitmap.c
+++ A05/bitmap.c
@@ -3,8 +3,19 @@
 int main() {
   unsigned long img;
   scanf(" %lx", &img);
-  printf("Image (unsigned long): %lx\n", img);
-
-  // todo: your code here
+  printf("Image (unsigned long): \n%lx\n", img); 
+  unsigned long x;
+  for (int i = 63; i >= 0; i--){
+    x = (img &(0x1ul << i))>>i;
+    if(x > 0){
+      printf("@");
+    }
+    else {
+      printf(" ");
+    }
+    if((i%8)==0){
+      printf("\n");
+    }
+  }
   return 0;
 }
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -5,9 +5,17 @@
 // implement one
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+ FILE* fp = fopen(filename, "wb");
+ fprintf(fp, "P6\n# Created by GIMP version 2.10.24 PNM plug-in\n%d %d\n225\n",w,h);
 
-}
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+ for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+       fwrite(&pixels[i*w +j].red, 1, 1, fp );
+       fwrite(&pixels[i*w +j].blue, 1, 1, fp );
+       fwrite(&pixels[i*w +j].green, 1, 1, fp );
+    }
+ }
+ fclose(fp);
 
 }
