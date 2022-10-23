db128b30b803a552a34c4464308c6093b91a5985
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-10-22 23:08:58 -0400

    completed

--- A05/glitch.c
+++ A05/glitch.c
@@ -9,9 +9,28 @@ int main(int argc, char** argv) {
     printf("usage: glitch <file.ppm>\n");
     return 0;
   }
+  int w, h;
+  struct ppm_pixel* pixels = read_ppm(argv[1],&w,&h);
+  
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      pixels[i*(w)+j].red = (pixels[i*(w)+j].red) << (rand() % 2);
+      pixels[i*(w)+j].green = (pixels[i*(w)+j].green) << (rand() % 2);
+      pixels[i*(w)+j].blue = (pixels[i*(w)+j].blue) << (rand() % 2);
+   } 
+  } 
+ 
+ //FILE *fp
+ //fp = fopen(<argv[1]>"glitch.ppm","wb")
+ 
+  char* new_file = malloc(strlen(argv[1])+10);
+  strcpy(new_file,argv[1]);
+  strcat(new_file,"-glitch.ppm");
 
-  // todo: your code here
-
+  write_ppm(new_file, pixels, w, h);
+  
+  free(pixels);
+  free(new_file);
   return 0;
 }
 
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -7,8 +7,7 @@
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
  FILE* fp = fopen(filename, "wb");
  fprintf(fp, "P6\n# Created by GIMP version 2.10.24 PNM plug-in\n%d %d\n225\n",w,h);
-
-
+ 
  for (int i = 0; i < h; i++) {
     for (int j = 0; j < w; j++) {
        fwrite(&pixels[i*w +j].red, 1, 1, fp );
