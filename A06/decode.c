7051e1ee6874e916b3434fd205f04820435b62ad
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-11-01 05:07:52 -0400

    wip

--- A06/decode.c
+++ A06/decode.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_ppm.h"
+#include <math.h>
+
+int dec(int arr[]){
+int value = 0;
+for (int i = 7; i >= 0; i--){
+    value = value + arr[i]*pow(2.0,(double)(7-i));
+  }
+printf("%c",(char)(value));
+return value;
+}
+
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -8,6 +19,42 @@ int main(int argc, char** argv) {
     return 0;
   }
 
+  int w, h;
+  int index = 0;
+  int binary[1000];
+  
+  struct ppm_pixel* pixels = read_ppm(argv[1],&w,&h);
+
+  for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      binary[index] = (pixels[i*(w)+j].red & 0b1);
+      index = index +1;
+      binary[index] = (pixels[i*(w)+j].green & 0b1);
+      index = index +1;
+      binary[index] = (pixels[i*(w)+j].blue & 0b1);
+      index = index +1;
+    }
+  }
+  
+  for(int x = 0; x < index; x++){
+      printf("%d",binary[x]);
+  }
+
+int decarr[8];
+printf("\n");
+
+for (int x = 0; x < index; x += 8){
+  decarr[0] = binary[x];
+  decarr[1] = binary[x+1];
+  decarr[2] = binary[x+2];
+  decarr[3] = binary[x+3];
+  decarr[4] = binary[x+4];
+  decarr[5] = binary[x+5];
+  decarr[6] = binary[x+6];
+  decarr[7] = binary[x+7];
+  dec(decarr);
+  }
+
+  free(pixels);
   return 0;
 }
-
--- A06/encode.c
+++ A06/encode.c
@@ -4,12 +4,66 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+int* bin(char* arr){
+  int* binary = (int*)malloc(8*sizeof(arr));
+  int num;
+  for (int i = 0; i < 8*(sizeof(arr)); i+=8){
+    num = arr[i/8];
+    binary[i+7] = num%2;
+    num = num/2;
+    binary[i+6] = num%2;
+    num = num/2;
+    binary[i+5] = num%2;
+    num = num/2;
+    binary[i+4] = num%2;
+    num = num/2;
+    binary[i+3] = num%2;
+    num = num/2;
+    binary[i+2] = num%2;
+    num = num/2;
+    binary[i+1] = num%2;
+    num = num/2;
+    binary[i] = num%2;
+  }
+return binary;
+}
+
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     printf("usage: encode <file.ppm>\n");
     return 0;
   }
 
+  int w, h;
+  int index = 0;
+  char message[100];
+   
+  struct ppm_pixel* pixels = read_ppm(argv[1],&w,&h);
+  printf("Please enter a secret message: ");
+  scanf("%s", message);
+  int *num = bin(message);
+  int y = 0;
+   for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      pixels[i*(w)+j].red = (pixels[i*(w)+j].red & 0b11111110) | num[y] ;
+      y = y +1;
+      pixels[i*(w)+j].green = (pixels[i*(w)+j].green & 0b11111110) | num[y];
+      y = y +1;
+      pixels[i*(w)+j].blue = (pixels[i*(w)+j].blue & 0b11111110) | num[y];
+      y = y +1;
+    }
+  }
+  
+  char* new_file = malloc(strlen(argv[1])+11);
+  strcpy(new_file,argv[1]);
+  new_file[strlen(new_file)-4] = '\0';
+  strcat(new_file,"-new.ppm");
+
+  write_ppm(new_file, pixels, w, h);
+
+  free(pixels);
+  free(new_file);
   return 0;
 }
 
