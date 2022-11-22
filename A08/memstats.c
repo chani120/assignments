d152a37be4574c452118a19be6ecb54ad730e3bf
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-11-21 21:53:49 -0500

    completed

--- A08/memstats.c
+++ A08/memstats.c
@@ -17,6 +17,35 @@ struct chunk {
 };
 
 void memstats(struct chunk* freelist, void* buffer[], int len) {
+  int totB = 0;
+  int usedB = 0;
+  int freeB = 0;  
+  int totM = 0;
+  int usedM = 0;
+  int freeM = 0;
+  int unused = 0;
+  struct chunk *fl = freelist;
+
+  while(fl!=NULL){
+    freeB++;
+    freeM = freeM + fl -> size;
+    fl = fl -> next;
+  }
+
+  for(int i =0; i<len; i++){
+    if(buffer[i] != NULL){
+      usedB++;
+      struct chunk *cnk = (struct chunk *)((strunk chunk *)buffer[i]-1);
+      usedM = usedM + cnk -> size;
+      unused = (cnk->size - cnk->used) + unused;
+    }
+  }
+
+  totM = usedM + freeM;
+  float unUtil = (float)unused/usedM;
+  totB = freeB + usedB;
+
+  printf("Total blocks: %d Free blocks: %d Used blocks: %d \nTotal memory allocated: %d Free memory: %d Used memory: %d \nUnderutilized memory: %.2f \n", totB, freeB, usedB, totM, freeM, usedM, unUtil)
 }
 
 int main ( int argc, char* argv[]) {
--- A08/mylloc_list.c
+++ A08/mylloc_list.c
@@ -4,17 +4,64 @@
 
 struct chunk {
   int size;
-  int used;
+  int used; //amount of memory in use
   struct chunk *next;
 };
 struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
   // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
-}
+  if(size ==0){
+    return NULL;
+  }
+  
+  struct chunk *prev = NULL;
+  struct chunk *next = flist;
+  
+  while(next != NULL){
+    if(next->size >= size){
+      if(prev != NULL){
+        prev->next= next->next;
+
+      }
+      else{
+        flist = next->next;
+      }
+
+      next->used = size;
+      return (void*)(next +1);
+    }
+    else{
+      prev = next;
+      next = next->next;
+
+    }
+  }
+  
+ void *memory = sbrk(size + sizeof(struct chunk));
+  if(memory == (void *)-1){
+    return NULL;
+  }
+  else{
+    struct chunk *c = (struct chunk*)memory;
+    c->size = size;
+    c->used = size;
+
+    return (void*)(c +1);
+  }
+
+ }
+  
 
 void free(void *memory) {
+  
   // TODO: Implement malloc with a free list (See reading for details)
+  if(memory!= NULL){
+
+    struct chunk *c= (struct chunk *)((struct chunk *) memory -1);
+    c->next = flist;
+    flist = c;
+  
   return;
+  }
 }
