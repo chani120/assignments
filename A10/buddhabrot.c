2e35303e67422ab660572acba6b0e75962cc5443
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-12-13 01:09:23 -0500

    done

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -7,12 +7,119 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+hread mutex_t mutex,
+pthread barrier_t barrier;
+
+struct thread_data{
+int col1;
+int col2;
+int row1;
+int row2;
+struct ppm_pixel *pal;
+int size;
+int maxIterations;
+float x_min;
+float y_min;
+float x_max;
+float y_max;
+int *mem;
+int *count;
+int *con;
+};
+
+void mandelbrot(struct ppm_pixel *pixels, int col1 , int col2, int row1, int row2, struct ppm_pixel *pal, int size, int maxIterations, float x_min, float y_min, float x_max, float y_max, int *mem, int *inter, int *con){
+  for(int i= row1; i<row2; i++){
+    for(int j=col1; j<col2; j++){
+      float x = 0.0;
+      float y = 0.0;
+      int c = 0;
+
+      float x_var = ((float) (i))/size;
+      float y_var = ((float) (j))/size;
+      float x0 = x_min + x_var * (x_max - x_min);
+      float y0 = y_min + y_var * (y_max - y_min);
+  
+      while (count < maxIterations && x*x + y*y < 2*2){
+        float temp_x = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = temp_x;
+        c++;
+      }
+    }
+    }
+  }
+
+ void formula2(int id, struct ppm_pixel *pix, int col1 , int col2, int row1, int row2, struct ppm_pixel *pal, int size, int maxIterations, float x_min, float y_min, float x_max, float y_max, int *mem, int *count, int *con){
+  for(int i= row1; i<row2; i++){
+    for(int j=col1; j<col2; j++){
+      float x = 0.0;
+      float y = 0.0;
+
+      float x_var = ((float) (i))/size;
+      float y_var = ((float) (j))/size;
+      float x0 = x_min + x_var * (x_max - x_min);
+      float y0 = y_min + y_var * (y_max - y_min);
+  
+      while (x*x + y*y < 2*2){
+        float temp_x = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = temp_x;
+
+        int yrow = round(size * (y - y_min)/(y_max - y_min));
+        int xcol = round(size * (x - x_min)/(x_max - x_min));
+        if(yrow<0 || yrow>=size){
+          continue;
+        }
+        if(xcol<0 || xcol>=size){
+          continue;
+        }
+        
+        pthread_mutex_lock(&mutex);
+        count(yrow * size + xcol) += 1;
+        if(count[yrow * size + xcol]> *con){
+          *con = count[yrow * size + xcol];
+        }
+        pthread_mutex_unlock(&mutex);
+      }
+    }
+  }
+ }
+
+void formula3(int id, struct ppm_pixel *pix, int col1 , int col2, int row1, int row2, struct ppm_pixel *pal, int size, int maxIterations, float x_min, float y_min, float x_max, float y_max, int *mem, int *count, int *con){
+  float gamma = 0.681;
+  float factor = 1.0/gamma;
+  for(int i= row1; i<row2; i++){
+    for(int j=col1; j<col2; j++){
+      float val = 0.0;
+      
+      if(count[i * size +j]>0){
+        val = log(count[i * size +j])/log(*con);
+        val = pow(val,factor);
+      }
+      pix[i * size + j].red = val * 255;
+      pix[i * size + j].green = val * 255;
+      pix[i * size + j].blue = val * 255;
+      }
+    }
+  }
+ 
+
+
+void *do_formula(void *userdata){
+  struct thread_data *data = (struct thread_data *) userdata;
+  formula1(data->id, data->pix, data->col1, data->col2, data->row1, data->row2, data->pal, data->size, data->maxIterations, data->x_min, data->x_max, data->y_max, data->y_min, data->mem, data->count, data ->con);
+  formula2(data->id, data->pix, data->col1, data->col2, data->row1, data->row2, data->pal, data->size, data->maxIterations, data->x_min, data->x_max, data->y_max, data->y_min, data->mem, data->count, data ->con);
+  pthread_barrier_wait(&barrier);
+  formula3(data->id, data->pix, data->col1, data->col2, data->row1, data->row2, data->pal, data->size, data->maxIterations, data->x_min, data->x_max, data->y_max, data->y_min, data->mem, data->count, data ->con);
+return (void*) NULL;
+}
+
 int main(int argc, char* argv[]) {
   int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
-  float ymin = -1.12;
-  float ymax = 1.12;
+  float x_min = -2.0;
+  float x_max = 0.47;
+  float y_min = -1.12;
+  float y_max = 1.12;
   int maxIterations = 1000;
   int numProcesses = 4;
 
@@ -20,19 +127,131 @@ int main(int argc, char* argv[]) {
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
     switch (opt) {
       case 's': size = atoi(optarg); break;
-      case 'l': xmin = atof(optarg); break;
-      case 'r': xmax = atof(optarg); break;
-      case 't': ymax = atof(optarg); break;
-      case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+      case 'l': x_min = atof(optarg); break;
+      case 'r': x_max = atof(optarg); break;
+      case 't': y_max = atof(optarg); break;
+      case 'b': y_min = atof(optarg); break;
+      case '?': printf("usage: %s -s <size> -l <x_min> -r <x_max> "
+        "-b <y_min> -t <y_max> -p <numProcesses>\n", argv[0]); break;
     }
   }
   printf("Generating mandelbrot with size %dx%d\n", size, size);
   printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+  printf("  X range = [%.4f,%.4f]\n", x_min, x_max);
+  printf("  Y range = [%.4f,%.4f]\n", y_min, y_max);
+  srand(time(0));
+  struct ppm_pixel *pal=malloc(sizeof(struct ppm_pixel)*maxIterations);
+  for(int i=0;i<maxIterations; i++){
+    pal[i].red=rand() %255;
+    pal[i].green=rand() %255;
+    pal[i].blue=rand() %255;
+  }
+  struct ppm_pixel *pix=malloc(sizeof(struct ppm_pixel)*size*size);
+  int *mem,*count=malloc(sizeof(int)*size*size);
+  for(int i=0;i<size;i++;){
+    for(int i=0;i<size;i++;){
+      count[i][j]=0
+    }
+  }
+  int con=0;
+  double t;
+  struct timeval tstart,tend;
+  gettimepfday(&tstart,NULL);
+  pthread_t threads[4];
+  pthread_mutex_init(&mutex, NULL);
+  pthread_barrier_init(&barrier,NULL,4);
+  struct thread_data data[4];
+  for(int i=0;i<4;i++){
+    if(i=0){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=1;
+      data[i].pix=pix;
+      data[i].col1=0;
+      data[i].col2=240;
+      data[i].row1=0;
+      data[i].row2=240;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].x_min=x_min;
+      data[i].y_min=y_min;
+      data[i].x_max=x_max;
+      data[i].y_max=y_max;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+    if(i=1){
+      printf("%d. Sub image block: collumns 240,480 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].col1=0;
+      data[i].col2=240;
+      data[i].row1=0;
+      data[i].row2=240;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].x_min=x_min;
+      data[i].y_min=y_min;
+      data[i].x_max=x_max;
+      data[i].y_max=y_max;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+if(i=2){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].col1=0;
+      data[i].col2=240;
+      data[i].row1=240;
+      data[i].row2=480;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].x_min=x_min;
+      data[i].y_min=y_min;
+      data[i].x_max=x_max;
+      data[i].y_max=y_max;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+if(i=3){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].col1=240;
+      data[i].col2=480;
+      data[i].row1=240;
+      data[i].row2=480;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].x_min=x_min;
+      data[i].y_min=y_min;
+      data[i].x_max=x_max;
+      data[i].y_max=y_max;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+  }
+  for(int i=0;i<4;){
+    pthread_join(threads[i],NULL);
+  }
+  gettimeofday(&tend,NULL);
+  t=tend.tv_sec-tstart.tv_sec+(tend.tv_usec-tstart.tv_usec)/1.e6;
+  int timestamp=time(0)
+  printf("Buddahbrot set %dx%d computed!It took %f seconds\n",size,size,t)
+  write_ppm(fp,pix,size,size);
+  pthread_mutex_destroy(&mutex);
+  pthread_barrier_destroy(&barrier);
+  free(pix);
+  free(pal);
+  free(mem);
+  free(count);
 
-  // todo: your code here
-  // compute image
 }
--- A10/thread_mandelbrot.c
+++ A10/thread_mandelbrot.c
@@ -7,33 +7,130 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+void thread_mand((void*)arguments){
+  struct args thread_args = (struct args)arguments;
+  long double xvar = 0;
+  long double yvar = 0;
+  long double x1 = 0;
+  long double y1 = 0;
+  int iteration = 0;
+  long double x = 0, 
+  long double y = 0;
+  long double xtemp = 0;
+  double timer = 0;
+  struct timeval timestart, timeend;
+ 
+  for (int i = 0; i < thread_args.size; i++){ // row
+    for (int j = 0; j <thread_args.size; j++){ //column
+      xvar = (float)i / thread_args.size;
+      yvar = (float)j / thread_args.size;
+      x1 = thread_args.x_min + (xvar * (thread_args.x_max - thread_args.x_min));
+      y1 = thread_args.y_min + (yvar * (thread_args.y_max - thread_args.y_min));
+
+      x = 0;
+      y = 0;
+      iteration = 0;
+
+      while((iteration < thread_args.maxIterations)&&(x*x + y*y < 4)){
+        xtemp = x*x - y*y + x1;
+        y = 2*x*y + y1;
+        x = xtemp;
+        iteration++;
+      }
+
+      if (iteration < thread_args.maxIterations){
+        thread_args.image[i][j] = thread_args.pal[iteration];
+      }
+      else{
+        thread_args.image[i][j].red = 0;
+        thread_args.image[i][j].green = 0;
+        thread_args.image[i][j].blue = 0;
+      }
+
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   int size = 480;
-  float xmin = -2.0;
-  float xmax = 0.47;
-  float ymin = -1.12;
-  float ymax = 1.12;
+  float x_min = -2.0;
+  float x_max = 0.47;
+  float y_min = -1.12;
+  float y_max = 1.12;
   int maxIterations = 1000;
   int numProcesses = 4;
+  int col1;
+  int col2;
+  int row1;
+  int row2;
+  struct ppm_pixel* pal;
+  struct ppm_pixel** image; 
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
     switch (opt) {
       case 's': size = atoi(optarg); break;
-      case 'l': xmin = atof(optarg); break;
-      case 'r': xmax = atof(optarg); break;
-      case 't': ymax = atof(optarg); break;
-      case 'b': ymin = atof(optarg); break;
-      case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+      case 'l': x_min = atof(optarg); break;
+      case 'r': x_max = atof(optarg); break;
+      case 't': y_max = atof(optarg); break;
+      case 'b': y_min = atof(optarg); break;
+      case '?': printf("usage: %s -s <size> -l <x_min> -r <x_max> "
+        "-b <y_min> -t <y_max> -p <numProcesses>\n", argv[0]); break;
     }
   }
   printf("Generating mandelbrot with size %dx%d\n", size, size);
   printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+  printf("  X range = [%.4f,%.4f]\n", x_min, x_max);
+  printf("  Y range = [%.4f,%.4f]\n", y_min, y_max);
 
-  // todo: your code here
-  // generate pallet
-  // compute image
+  struct ppm_pixel** image = (struct ppm_pixel**)malloc(size*size*sizeof(struct ppm_pixel));
+  for(int i = 0; i < size; i++){
+    image[i] = (struct ppm_pixel*)malloc(size*sizeof(struct ppm_pixel));
+  }
+
+
+  srand(time(0));
+  struct ppm_pixel* pal = (struct ppm_pixel*)malloc(maxIterations*sizeof(struct ppm_pixel));
+
+  for (int i = 0; i < maxIterations; i++){
+    pal[i].red = 100 + (rand() % 155);
+    pal[i].green = rand() % 150;
+    pal[i].blue = 100 + (rand() % 155);
+  }
+  pthread thread1_id;
+  struct args args1;
+  args1.col1 = 0;
+  args1.row1 = 0;
+  args1.col2 = 240;
+  args1.row2 = 240;
+  args1.size = 400;
+  args1.maxIterations = 1000;
+  args1.pal = pal;
+  args1.image = image;
+  args1.x_max = x_max;
+  args1.y_max = y_max;
+  args1.x_min = x_min;
+  args1.y_min = y_min;
+  pthread_create(&thread1_id, NULL, thread_mand, args1);
+  pthread thread2_id;
+  struct args args2;
+  pthread_create(&thread2_id, NULL, thread_mand, args2);
+  pthread thread3_id;
+  struct args args3;
+  pthread_create(&thread3_id, NULL, thread_mand, args3);
+  pthread_join(thread1_id, NULL);
+  pthread_join(thread2_id, NULL);
+  pthread_join(thread3_id, NULL);
+
+
+  printf("Computed mandelbrot set (%d,%d)\n", size, size); \
+  const char* outputfile = "testt.ppm\0";
+  write_ppm_2d(outputfile, image, size, size);
+  printf("Writing to file: %s\n", outputfile);
+  for(int i = 0; i < size; i++){
+    free(image[i]);
+  }
+  free(image);
+  free(pal);
 }
+
