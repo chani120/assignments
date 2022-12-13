4bb7acdea0c1544d386743a8911dd50aeccf6599
Chandini Ragobar <cragobar@goldengate.cs.brynmawr.edu>
2022-12-13 01:10:07 -0500

    done

--- A10/write_ppm.c
+++ A10/write_ppm.c
@@ -2,8 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-}
+// implement one
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+ FILE* fp = fopen(filename, "wb");
+ fprintf(fp, "P6\n# Created by GIMP version 2.10.24 PNM plug-in\n%d %d\n255\n",w,h);
+ 
+ for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+       fwrite(&pixels[i*w +j].red, 1, 1, fp );
+       fwrite(&pixels[i*w +j].blue, 1, 1, fp );
+       fwrite(&pixels[i*w +j].green, 1, 1, fp );
+    }
+ }
+ fclose(fp);
+
 }
